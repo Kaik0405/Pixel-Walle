52ef5049ea816bcea9b537008d7918524201c2b0
Kaik0405 <159601515+Kaik0405@users.noreply.github.com>
2025-04-10 11:32:16 -0400

    Building the Lexer Class

--- Pixel_Walle/Interpreter/Lexer/Lexer.cs
+++ Pixel_Walle/Interpreter/Lexer/Lexer.cs
@@ -8,6 +8,87 @@ namespace Pixel_Walle
 {
     public class Lexer
     {
+        private static Dictionary<string, Token.TokenType> KeyWords = new Dictionary<string, Token.TokenType>()
+        {
+            //KeyWords
+            {"Spawn",Token.TokenType.Spawn},
+            {"Color",Token.TokenType.Color},
+            {"Size",Token.TokenType.Size},
+            {"DrawLine",Token.TokenType.DrawLine},
+            {"DrawCircle",Token.TokenType.DrawCircle},
+            {"DrawRectangle",Token.TokenType.DrawRectangle},
+            {"Fill",Token.TokenType.Fill},
+            {"GoTo",Token.TokenType.GoTo},
+            {"GetActualX",Token.TokenType.GetActualX},
+            {"GetActualY",Token.TokenType.GetActualY},
+            {"GetCanvasSize",Token.TokenType.GetCanvasSize},
+            {"GetColorCount",Token.TokenType.GetColorCount},
+            {"IsBrushColor",Token.TokenType.IsBrushColor},
+            {"IsBrushSize",Token.TokenType.IsBrushSize},
+            {"IsCanvasColor",Token.TokenType.IsCanvasColor},
+
+            //Colors
+            {"Red",Token.TokenType.Red},
+            {"Blue",Token.TokenType.Blue},
+            {"Green",Token.TokenType.Green},
+            {"Yellow",Token.TokenType.Yellow},
+            {"Orange",Token.TokenType.Orange},
+            {"Purple",Token.TokenType.Purple},
+            {"Black",Token.TokenType.Black},
+            {"White",Token.TokenType.White},
+            {"Transparent",Token.TokenType.Transparent},
+
+            // Símbolos
+            {"\\(", Token.TokenType.OpenParan},
+            {"\\)", Token.TokenType.ClosedParan},
+            {"\\[", Token.TokenType.OpenBracket},
+            {"\\]", Token.TokenType.ClosedBracket},
+            {"\"", Token.TokenType.Quote},
+            {"<-", Token.TokenType.Assignment},
+            {"<=", Token.TokenType.LessThanEqual},
+            {">=", Token.TokenType.GreaterThanEqual},
+            {"<", Token.TokenType.LessThan},
+            {">", Token.TokenType.GreaterThan},
+            {"==", Token.TokenType.Equal},
+            {"=>", Token.TokenType.Arrow},
+            {"\\:", Token.TokenType.Colon},
+            {"\\.", Token.TokenType.Dot},
+            {",", Token.TokenType.Comma},
+            {";", Token.TokenType.SemiColon},
+
+            //Operadores Aritméticos
+            {"\\*\\*", Token.TokenType.Pow},
+            {"\\+", Token.TokenType.Plus},
+            {"-",Token.TokenType.Minus },
+            {"\\*", Token.TokenType.Times},
+            {"/",Token.TokenType.Divide },
+            {"\\^",Token.TokenType.Pow },
+            {"\\%",Token.TokenType.Module },
+
+            // Operadores Lógicos
+            {"&&", Token.TokenType.AND},
+            {"\\|\\|", Token.TokenType.OR},
+
+            //Bolean
+            {"True", Token.TokenType.True},
+            {"False", Token.TokenType.False},
+
+            // Concatenación
+            {"@@", Token.TokenType.ATAT},
+            {"@", Token.TokenType.AT},
+
+             // Keys Arbitrarias
+            {"[a-zA-Z_]\\w*", Token.TokenType.UnKnown},
+            {"\\d+", Token.TokenType.Digit},
+        };
+        private string[]? code;
+        private List<Token> tokensList;
+        public string[] Code { get; private set; }
+        public Lexer(string[] code)
+        {
+            this.code = code;
+            tokensList = new List<Token>();
+        }
 
     }
 }
